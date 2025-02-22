ec41fbaaefdb5827203b163abf8a8bdaee1f0767
Alex Kavanagh <alexkav47@gmail.com>
2025-02-22 16:10:44 +0000

    Setup player placeholder and make movement work

--- Player.js
+++ Player.js
@@ -4,13 +4,25 @@ class Player{
 		this.x = 50;
 		this.y = 400;
 		this.ctx = ctx;
+		this.velocityY = 0;
 	}
 
 	update(deltaTime){
-
+		this.draw();
+		
 	}
 
-	onClick(){
-
+	draw(){
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,50,0,Math.PI*2);
+		ctx.fillStyle = "green";
+		ctx.fill();
+		ctx.stroke();
 	}
+
+	setPosition(pos){
+		this.y = pos;
+	}
+
+
 }
--- main.js
+++ main.js
@@ -3,11 +3,20 @@ const ctx = canvas.getContext("2d");
 
 
 const player = new Player(ctx);
+const rect = canvas.getBoundingClientRect();
+const scaleX = canvas.width/rect.width;
+const scaleY = canvas.height/rect.height;
 
-canvas.addEventListener('click', player.onClick);
+canvas.addEventListener('mousemove', (event)=>{
+	const mouseX = (event.clientX - rect.left) * scaleX;
+	const mouseY = (event.clientY - rect.top) * scaleY;
+
+	player.setPosition(mouseY);
+});
 
 let lastTime = 0;
 function update(time){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	const deltaTime = time - lastTime;
 	lastTime = time;
 	player.update(deltaTime);
