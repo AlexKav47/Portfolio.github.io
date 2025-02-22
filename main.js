4e3d76bf150828b5ba980fd42ccb4163c90b0210
Alex Kavanagh <alexkav47@gmail.com>
2025-02-22 17:54:18 +0000

    Add game states to main.js

--- main.js
+++ main.js
@@ -14,20 +14,48 @@ canvas.addEventListener('mousemove', (event)=>{
 	player.setPosition(mouseY);
 });
 
+
 const spawner = new AsteroidSpawner(ctx,player);
 
 let lastTime = 0;
-function update(time){
+var gameState = "start";
+canvas.addEventListener("click", (event)=>{
+	if(gameState === "start"){
+		gameState = "gameLoop";
+	}
+
+});
+
+function startScreen(time){
+	ctx.strokeStyle = "red";  
+	ctx.lineWidth = 2;  
+	ctx.font = "50px serif";
+	ctx.strokeText("Click the screen to start", canvas.width / 2 -200, canvas.height / 2, 400);
+}
+
+function gameLoopScreen(time){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	const deltaTime = time - lastTime;
 	lastTime = time;
 	player.update(deltaTime);
 	spawner.update(deltaTime);
 	spawner.draw();
-	window.requestAnimationFrame(update);
+}
+
+function gameOverScreen(time){
+
 }
 
 
+function update(time){
+	if(gameState == "start"){
+		startScreen(time);
+	}
+	else if(gameState == "gameLoop"){
+		gameLoopScreen(time);
+	}
+	window.requestAnimationFrame(update);
+}
 window.requestAnimationFrame((time)=>{
 	update(time);
 });
